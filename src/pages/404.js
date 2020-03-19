295c6e1f56000a84947bc7ddae000020c19e7edc
hendra sadewa <hendra.sadewa@dattabot.io>
2020-03-19 14:36:32 +0700

    initialize project

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Are you lost?</h1>
+    <p>Page that you requested is doesn't exists . . .</p>
   </Layout>
 )
 
