2876228fc7a5a43155c008a13a3a760377f2c33e
hendra sadewa <hendrasadewa30@gmail.com>
2020-05-11 21:07:42 +0700

    redesign article card

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,19 +1,46 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Box, Flex, Text } from 'rebass';
+import moment from 'moment';
 
 function ArticleCard({ id, slug, heroImage, title, date, description }) {
+  const datePublished = moment(date).format('dddd, D-MMMM-YYYY');
+
   return (
     <Link to={slug} style={{ textDecoration: 'none' }} key={id}>
-      <div>
-        <div>
-          <img src={heroImage} alt={title} async="on" />
-        </div>
-        <div>
-          <span>{date}</span>
-          <span>{title}</span>
-          <span>{description}</span>
-        </div>
-      </div>
+      <Flex flexDirection="row" mb={3}>
+        <Box
+          sx={{
+            backgroundImage: `url(${heroImage})`,
+            backgroundSize: 'cover',
+            minHeight: [120],
+            minWidth: [120],
+          }}
+        />
+        <Flex
+          flexDirection="column"
+          px={2}
+          justifyContent="space-between"
+          alignItems="flex-start"
+        >
+          <Flex flexDirection="column">
+            <Text
+              fontFamily="heading"
+              color="text"
+              fontWeight="heading"
+              fontSize={3}
+            >
+              {title}
+            </Text>
+            <Text fontFamily="body" color="text" py={1}>
+              {description}
+            </Text>
+          </Flex>
+          <Text fontFamily="body" color="text">
+            {datePublished}
+          </Text>
+        </Flex>
+      </Flex>
     </Link>
   );
 }
