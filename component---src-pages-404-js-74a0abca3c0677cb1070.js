889d7915ee740721ca022bfb2e75de2dbe57e04b
hendra sadewa <hendrasadewa30@gmail.com>
2021-02-06 17:05:06 +0700

    Updates

--- component---src-pages-404-js-74a0abca3c0677cb1070.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("qee6");t.default=function(){return l.a.createElement("div",null,l.a.createElement(u.a,{title:"404"}),l.a.createElement("h1",null,"Are you lost?"),l.a.createElement("p",null,"Page that you requested is doesn't exists . . ."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-74a0abca3c0677cb1070.js.map
